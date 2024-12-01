a98c774aafb88560b81192b4177868b0950e7682
Peyton11 <peyton.gardner03@gmail.com>
2024-12-01 14:50:57 -0500

    Add graph to webpage

--- pages/api/getData.js
+++ pages/api/getData.js
@@ -2,10 +2,11 @@ import StockData from '../../models/StockData';
 import { connect } from '../../lib/mongodb';
 
 export default async function handler(req, res) {
-  await connect();
+  // await connect();
 
   try {
     const stockData = await StockData.find({});
+    console.log("Database: ", stockData);
     res.status(200).json(stockData);
   } catch (error) {
     console.error('Error querying data:', error);
